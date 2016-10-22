47df6814a7eb4bcdbfe902e5f9b17374b3e9a384
chcamp <c_campos_cca@hotmail.com>
2016-10-22 02:33:21 -0500

    a ver si ahora funciona

--- services/src/main/java/com/joshlong/spring/walkingtour/services/model/Constants.java
+++ services/src/main/java/com/joshlong/spring/walkingtour/services/model/Constants.java
@@ -1,6 +1,11 @@
 package com.joshlong.spring.walkingtour.services.model;
 
 
+
+/*
+Clase de constantes de spring
+
+*/
 public class Constants {
     public static final String NAMESPACE = "http://samples.springsource.org/sawt/customers/schemas";
 }
